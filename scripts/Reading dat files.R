3eb0b8f234a81a2eca7313f9434b5f1a43e67058
MiguelAngelCollado <xmiguelangelcollado@gmail.com>
2017-10-06 14:29:00 +0200

    I'm starting to learn how to import .dat files and search for states

--- scripts/Reading dat files.R
+++ scripts/Reading dat files.R
@@ -1,1 +1,13 @@
-#Test for reading .dat
+#Reading .dat
+temp<-read.table("data/OC12.2.dat", skip = 24, sep = ",")
+temp<-as.data.frame(temp)
+
+#NOTE: Be careful, the states are named after a blank space ex: " p", " 1", " u"
+summary(temp$V2)
+levels(temp$V2)
+
+#We search for the earliest eating
+min(which(temp$V2 == " p"))
+
+
+
