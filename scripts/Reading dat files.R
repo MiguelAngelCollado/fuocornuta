f6ec9409d33fcc7bc8ac690ed7cd2722012e99c1
MiguelAngelCollado <xmiguelangelcollado@gmail.com>
2017-10-09 14:34:03 +0200

    Uploaded script about importing .dat and cutting below eating events
    Uploaded data cut before eating events

--- scripts/Reading dat files.R
+++ scripts/Reading dat files.R
@@ -1,3 +1,7 @@
+#Single file reading and cutting----
+#What we want to do in this script is to import .dat data and cut the test where eating
+#is involved, to cut below the action of eating because the behavior could be modified
+#by motivation or fast metabolism
 getwd()
 #Reading .dat
 temp<-read.table("data/OC12.2.dat", skip = 24, sep = ",")
@@ -10,5 +14,67 @@ levels(temp$V2)
 cut<-min(which(temp$V2 == " p"))
 temp2<-temp[1:cut,]
 
-write.table(temp2, file = "data/OC12.2c.dat")
+write.table(temp2, file = "data/databeforeeating/OC12.2c.dat")
 
+
+#Base for the loop----
+
+n=12
+temp<-read.table(paste0("data/OC",n,".2.dat"), skip = 24, sep = ",")
+cut<-min(which(temp$V2 == " p"))
+temp2<-temp[1:cut,]
+write.table(temp2, file = paste0("data/databeforeeating/OC",n,".2test.dat"))
+
+
+#Loop check----
+n=1
+n=NULL
+cut=NULL
+for (n in 1:146) {
+temp<-read.table(paste0("data/OC",n,".2.dat"), skip = 24, sep = ",")
+cut<-min(which(temp$V2 == " p"))
+  if(cut==Inf){
+    temp=NULL
+    temp2=NULL
+}else{
+  temp2<-temp[1:cut,]
+  write.table(temp2, file = paste0("data/databeforeeating/OC",n,".2cut.dat"))
+  temp=NULL
+  temp2=NULL
+  }
+}
+
+
+#####Loop for OCXX.2----
+#I modified the loop to ignore the errors to continue
+n=NULL
+cut=NULL
+for (n in 1:146) {
+  tryCatch(temp<-read.table(paste0("data/OC",n,".2.dat"), skip = 24, sep = ","), error=function(e){})
+  cut<-min(which(temp$V2 == " p"))
+  if(cut==Inf){
+    temp=NULL
+    temp2=NULL
+  }else{
+    temp2<-temp[1:cut,]
+    write.table(temp2, file = paste0("data/databeforeeating/OC",n,".2cut.dat"))
+    temp=NULL
+    temp2=NULL
+  }
+}
+###Loop for OCXX.3----
+n=NULL
+cut=NULL
+for (n in 1:146) {
+  tryCatch(temp<-read.table(paste0("data/OC",n,".3.dat"), skip = 24, sep = ","), error=function(e){})
+  cut<-min(which(temp$V2 == " p"))
+  if(cut==Inf){
+    temp=NULL
+    temp2=NULL
+  }else{
+    temp2<-temp[1:cut,]
+    write.table(temp2, file = paste0("data/databeforeeating/OC",n,".3cut.dat"))
+    temp=NULL
+    temp2=NULL
+  }
+}
