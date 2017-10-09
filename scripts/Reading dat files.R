f927a0a6a75d13be32887f20fe0bbc63ec4fc81a
MiguelAngelCollado <xmiguelangelcollado@gmail.com>
2017-10-09 09:22:14 +0200

    reading and cutting without loops for now

--- scripts/Reading dat files.R
+++ scripts/Reading dat files.R
@@ -1,13 +1,14 @@
+getwd()
 #Reading .dat
 temp<-read.table("data/OC12.2.dat", skip = 24, sep = ",")
-temp<-as.data.frame(temp)
-
+temp
 #NOTE: Be careful, the states are named after a blank space ex: " p", " 1", " u"
 summary(temp$V2)
 levels(temp$V2)
 
 #We search for the earliest eating
-min(which(temp$V2 == " p"))
+cut<-min(which(temp$V2 == " p"))
+temp2<-temp[1:cut,]
 
-
+write.table(temp2, file = "data/OC12.2c.dat")
 
